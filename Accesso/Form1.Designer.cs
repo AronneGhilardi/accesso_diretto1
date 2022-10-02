a31e8ad95311c626711e5c766dd953d59b520c41
Computer <Computer@DESKTOP-RSRR3D0>
2022-10-02 12:28:03 +0200

    Aggiungere i file di progetto.

---
+++ Accesso/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Accesso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1_ingresso = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label_nome = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1_ingresso
+            // 
+            this.textBox1_ingresso.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1_ingresso.Location = new System.Drawing.Point(12, 12);
+            this.textBox1_ingresso.Multiline = true;
+            this.textBox1_ingresso.Name = "textBox1_ingresso";
+            this.textBox1_ingresso.Size = new System.Drawing.Size(229, 22);
+            this.textBox1_ingresso.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Bahnschrift", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(247, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label_nome
+            // 
+            this.label_nome.AutoSize = true;
+            this.label_nome.Font = new System.Drawing.Font("Adobe Caslon Pro Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_nome.Location = new System.Drawing.Point(11, 50);
+            this.label_nome.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_nome.Name = "label_nome";
+            this.label_nome.Size = new System.Drawing.Size(70, 32);
+            this.label_nome.TabIndex = 5;
+            this.label_nome.Text = "Nome:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Inserisci località per trovare il locale associato";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 81);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_nome);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1_ingresso);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1_ingresso;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_nome;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Accesso/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Accesso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string filename = "File.csv";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cerca = textBox1_ingresso.Text.ToUpper();
+            label_nome.Text = ("nome: " + Ricerca(filename, cerca));
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        static string Ricerca(string filename, string nomecercato)
+        {
+            string line = "";
+            var f = new FileStream(filename, FileMode.Open, FileAccess.ReadWrite);
+            BinaryReader reader = new BinaryReader(f);
+            BinaryWriter writer = new BinaryWriter(f);
+            int tot = Convert.ToInt32(f.Length);
+            int length = 528;
+            tot /= 528;
+
+            string result = "";
+
+            int lung = Convert.ToInt32(f.Length);
+            int i = 0, j = tot - 1, m, pos = -1;
+
+            do
+            {
+                m = (i + j) / 2;
+                f.Seek(m * length, SeekOrigin.Begin);
+                line = Encoding.ASCII.GetString(reader.ReadBytes(length));
+                result = FromString(line, 0);
+
+                if (myCompare(result, nomecercato) == 0)
+                {
+                    pos = m;
+                }
+                else if (myCompare(result, nomecercato) == -1)
+                {
+                    i = m + 1;
+                }
+                else
+                    j = m - 1;
+
+
+            } while (i <= j && pos == -1);
+
+            if (pos != -1)
+                MessageBox.Show("Campo trovato in posizione: " + pos);
+            else
+                throw new Exception("Errore! Campo inesistente.");
+            string fine = FromString(line, 7);
+            f.Close();
+            return fine;
+
+        }
+
+        static int myCompare(string stringa1, string stringa2)
+        {
+            if (stringa1 == stringa2)
+                return 0;
+
+            char[] char1 = stringa1.ToCharArray();
+            char[] char2 = stringa2.ToCharArray();
+            int l = char1.Length;
+            if (char2.Length < l)
+                l = char2.Length;
+
+            for (int i = 0; i < l; i++)
+            {
+                if (char1[i] < char2[i])
+                    return -1;
+                if (char1[i] > char2[i])
+                    return 1;
+            }
+
+            return 0;
+        }
+
+
+
+        public static string FromString(string Stringa, int pos, string sep = ";")
+        {
+            string[] ris = Stringa.Split(';');
+            return ris[pos];
+        }
+    }
+}
